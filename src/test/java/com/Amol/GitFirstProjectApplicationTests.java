e47d587ac8c2e8e5cbfbd37353cb51f1d68d55e3
BalajiNagargoje <balajinagargoje2410@gmail.com>
2021-10-19 10:20:07 +0530

    initial commit

---
+++ src/test/java/com/Amol/GitFirstProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Amol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitFirstProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
